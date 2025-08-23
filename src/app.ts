5fb9ad2c12393e262854ef74b0b538622b68763f
Pasindu Sampath <pasindub32@gmail.com>
2025-08-23 21:52:35 +0530

    implemented routes

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,10 @@
 import express, { Application } from "express";
- 
+import greetingRoute from "./routes/greeting.route";
 
 const app:Application = express();
 
 app.use(express.json());
-
+app.use("/greeting" ,greetingRoute)
 app.get("/", (req, res) => {
     res.send("Hello World");
 });
