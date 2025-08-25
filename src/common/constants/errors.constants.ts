e030c131f33cb71b8e65f1474a44b9672b725d1e
Pasindu Sampath <pasindub32@gmail.com>
2025-08-25 21:12:06 +0530

    Remove dist directory and its contents; update .gitignore to exclude dist folder.

---
+++ src/common/constants/errors.constants.ts
@@ -0,0 +1,14 @@
+export const ERRORS = {
+    BAD_REQUEST : {
+        key: 'BAD_REQUEST',
+        message: 'Invalid syntax or missing parameters'
+    },
+    UNAUTHORIZED : {
+        key: 'UNAUTHORIZED',
+        message: 'You are not allowed to access this resource'
+    },
+    LOGIN_FAILED : {
+        key: 'LOGIN_FAILED',
+        message: 'Invalid username or password'
+    }
+}
