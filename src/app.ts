487eb2a60259c61f85299fd12378106ce6e049a9
Pasindu Sampath <pasindub32@gmail.com>
2025-08-25 21:53:35 +0530

    Update configuration to use environment variables; add dotenv for managing environment settings and refactor app to utilize APP_CONFIG for server port.

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,13 @@
 import express, { Application } from "express";
 import { Routes } from "./routes/routes";
+import { APP_CONFIG } from "./config/app.config";
 
 const app:Application = express();
 
 app.use(express.json());
 app.use("/api" ,Routes.getInstance().router)
 
-const PORT = 3000;
+const PORT = APP_CONFIG.PORT;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
